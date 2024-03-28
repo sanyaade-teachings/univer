d21f4a17a8b93589224a1a08aa8400f55d124ac2
gggpound <gpoundLiu@gmail.com>
2024-03-28 21:43:53 +0800

    wait

--- packages/sheets-conditional-formatting/src/plugin.ts
+++ packages/sheets-conditional-formatting/src/plugin.ts
@@ -17,10 +17,10 @@
 import { ICommandService, Plugin, PluginType } from '@univerjs/core';
 import { Inject, Injector } from '@wendellhu/redi';
 import { SHEET_CONDITIONAL_FORMATTING_PLUGIN } from './base/const';
-import { ConditionalFormattingService } from './services/conditional-formatting.service';
-import { ConditionalFormattingRuleModel } from './models/conditional-formatting-rule-model';
-import { ConditionalFormattingViewModel } from './models/conditional-formatting-view-model';
-import { RenderController } from './controllers/cf.render.controller';
+// import { ConditionalFormattingService } from './services/conditional-formatting.service';
+// import { ConditionalFormattingRuleModel } from './models/conditional-formatting-rule-model';
+// import { ConditionalFormattingViewModel } from './models/conditional-formatting-view-model';
+// import { RenderController } from './controllers/cf.render.controller';
 import { DddAverageCfCommand } from './commands/commands/add-average-cf.command';
 import { AddColorScaleConditionalRuleCommand } from './commands/commands/add-color-scale-cf.command';
 import { AddDataBarConditionalRuleCommand } from './commands/commands/add-data-bar-cf.command';
@@ -32,25 +32,25 @@ import { AddTimePeriodCfCommand } from './commands/commands/add-time-period-cf.c
 import { AddUniqueValuesCfCommand } from './commands/commands/add-unique-values-cf.command';
 import { AddConditionalRuleMutation } from './commands/mutations/add-conditional-rule.mutation';
 import { DeleteConditionalRuleMutation } from './commands/mutations/delete-conditional-rule.mutation';
-import { RefRangeController } from './controllers/cf.ref-range.controller';
+// import { RefRangeController } from './controllers/cf.ref-range.controller';
 import { SetConditionalRuleMutation } from './commands/mutations/set-conditional-rule.mutation';
-import { ConditionalFormattingCopyPasteController } from './controllers/cf.copy-paste.controller';
-import { ConditionalFormattingAutoFillController } from './controllers/cf.auto-fill.controller';
+// import { ConditionalFormattingCopyPasteController } from './controllers/cf.copy-paste.controller';
+// import { ConditionalFormattingAutoFillController } from './controllers/cf.auto-fill.controller';
 // import { ConditionalFormattingMenuController } from './controllers/cf.menu.controller';
-import { ConditionalFormattingI18nController } from './controllers/cf.i18n.controller';
+// import { ConditionalFormattingI18nController } from './controllers/cf.i18n.controller';
 
 import { ClearRangeCfCommand } from './commands/commands/clear-range-cf.command';
 import { ClearWorksheetCfCommand } from './commands/commands/clear-worksheet-cf.command';
-import { OpenConditionalFormattingOperator } from './commands/operations/open-conditional-formatting-panel';
+// import { OpenConditionalFormattingOperator } from './commands/operations/open-conditional-formatting-panel';
 import { DeleteCfCommand } from './commands/commands/delete-cf.command';
 import { SetCfCommand } from './commands/commands/set-cf.command';
 import { moveCfCommand } from './commands/commands/move-cf.command';
 import { AddCfCommand } from './commands/commands/add-cf.command';
 import { MoveConditionalRuleMutation } from './commands/mutations/move-conditional-rule.mutation';
-import { ConditionalFormattingFormulaService } from './services/conditional-formatting-formula.service';
+// import { ConditionalFormattingFormulaService } from './services/conditional-formatting-formula.service';
 import { ConditionalFormattingFormulaMarkDirty } from './commands/mutations/formula-mark-dirty.mutation';
-import { ConditionalFormattingEditorController } from './controllers/cf.editor.controller';
-import { ConditionalFormattingClearController } from './controllers/cf.clear.controller';
+// import { ConditionalFormattingEditorController } from './controllers/cf.editor.controller';
+// import { ConditionalFormattingClearController } from './controllers/cf.clear.controller';
 
 export class SheetsConditionalFormattingPlugin extends Plugin {
     static override type = PluginType.Sheet;
@@ -63,7 +63,7 @@ export class SheetsConditionalFormattingPlugin extends Plugin {
         AddTextCfCommand,
         AddTimePeriodCfCommand,
         AddUniqueValuesCfCommand,
-        OpenConditionalFormattingOperator,
+        // OpenConditionalFormattingOperator,
         DeleteCfCommand,
         SetCfCommand,
         moveCfCommand,
@@ -83,20 +83,20 @@ export class SheetsConditionalFormattingPlugin extends Plugin {
     }
 
     override onStarting(): void {
-        this._injector.add([ConditionalFormattingService]);
-        this._injector.add([ConditionalFormattingFormulaService]);
+        // this._injector.add([ConditionalFormattingService]);
+        // this._injector.add([ConditionalFormattingFormulaService]);
 
-        this._injector.add([ConditionalFormattingRuleModel]);
-        this._injector.add([ConditionalFormattingViewModel]);
+        // this._injector.add([ConditionalFormattingRuleModel]);
+        // this._injector.add([ConditionalFormattingViewModel]);
 
-        this._injector.add([RenderController]);
-        this._injector.add([RefRangeController]);
-        this._injector.add([ConditionalFormattingCopyPasteController]);
-        this._injector.add([ConditionalFormattingAutoFillController]);
+        // this._injector.add([RenderController]);
+        // this._injector.add([RefRangeController]);
+        // this._injector.add([ConditionalFormattingCopyPasteController]);
+        // this._injector.add([ConditionalFormattingAutoFillController]);
         // this._injector.add([ConditionalFormattingMenuController]);
-        this._injector.add([ConditionalFormattingI18nController]);
-        this._injector.add([ConditionalFormattingEditorController]);
-        this._injector.add([ConditionalFormattingClearController]);
+        // this._injector.add([ConditionalFormattingI18nController]);
+        // this._injector.add([ConditionalFormattingEditorController]);
+        // this._injector.add([ConditionalFormattingClearController]);
     }
 
     _initCommand() {
