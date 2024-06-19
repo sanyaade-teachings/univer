5db0a599e204e9c407e49ed532a09566c0baa5a7
lumixraku <lumixraku@gmail.com>
2024-06-19 20:55:14 +0800

    chore: move dash logic into dashedrect

--- packages/sheets-ui/src/services/selection/selection-shape.ts
+++ packages/sheets-ui/src/services/selection/selection-shape.ts
@@ -17,7 +17,7 @@
 import type { IRangeWithCoord, ISelectionCellWithCoord, Nullable, ThemeService } from '@univerjs/core';
 import { ColorKit, Disposable, RANGE_TYPE, toDisposable } from '@univerjs/core';
 import type { Scene } from '@univerjs/engine-render';
-import { cancelRequestFrame, FIX_ONE_PIXEL_BLUR_OFFSET, Group, Rect, requestNewFrame, TRANSFORM_CHANGE_OBSERVABLE_TYPE } from '@univerjs/engine-render';
+import { cancelRequestFrame, DashedRect, FIX_ONE_PIXEL_BLUR_OFFSET, Group, Rect, requestNewFrame, TRANSFORM_CHANGE_OBSERVABLE_TYPE } from '@univerjs/engine-render';
 import type { ISelectionStyle, ISelectionWidgetConfig, ISelectionWithCoordAndStyle } from '@univerjs/sheets';
 import {
     getNormalSelectionStyle,
@@ -586,7 +586,7 @@ export class SelectionShape extends Disposable {
             zIndex: zIndex + 1,
         });
 
-        this._dashRect = new Rect(SELECTION_MANAGER_KEY.dash + zIndex, {
+        this._dashRect = new DashedRect(SELECTION_MANAGER_KEY.dash + zIndex, {
             zIndex: zIndex + 2,
             evented: false,
             stroke: '#fff',
